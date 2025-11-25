2f23fe01cb5e414932d7fa17c6620ba6a17f6240
Sicanet-UzielZarate <uziel.zarate@sicanetsc.com>
2025-11-25 12:17:44 -0600

    Reset database connection credentials
    Updated database connection parameters to empty strings.

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'tinderito'
+    user: '',
+    password: '',
+    database: ''
 });
 
 connection.connect(err => {
